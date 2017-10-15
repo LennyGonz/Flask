eb4786c6d7e0f6a8133ba394a7404945c80a276c
Lennygonz <lenny.gonzaleznyc@gmail.com>
2017-10-15 15:08:29 -0400

    mapping multiple urls

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import render_template  #YOU HAVE TO ADD THIS TO RENDER TEMPLATES
 
 app = Flask(__name__)
 
-@app.route('/profile/<name>')
-def profile(name):
-    return render_template("profile.html", name=name) # we need to insert the name of the html file
-                                                      # we also need to include our name variable
+@app.route('/')
+@app.route('/<user>')
+def index(user=None):
+    return render_template("user.html",user=user)
+
 if __name__ == "__main__":
     app.run()
